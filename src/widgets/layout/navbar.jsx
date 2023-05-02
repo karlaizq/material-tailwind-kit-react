28a6c83d7e6ab62451839c2a89a65203a647a5ee
KDI2 <ki6@njit.edu>
2023-05-02 16:12:53 -0400

    changes

--- src/widgets/layout/navbar.jsx
+++ src/widgets/layout/navbar.jsx
@@ -1,143 +1,186 @@
 import React from "react";
 import {
-  Navbar as MTNavbar,
-  MobileNav,
+  Card,
+  CardBody,
+  CardHeader,
   Typography,
   Button,
   IconButton,
+  Input,
+  Textarea,
 } from "@material-tailwind/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { UsersIcon } from "@heroicons/react/24/solid";
+import { PageTitle, Footer } from "@/widgets/layout";
+import { FeatureCard, TeamCard } from "@/widgets/cards";
+import { featuresData, teamData, contactData } from "@/data";
 
-export function Navbar({ brandName, routes, action }) {
-  const [openNav, setOpenNav] = React.useState(false);
-
-  React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false)
-    );
-  }, []);
-
-  const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 text-inherit lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      {routes.map(({ name, path, icon, href, target }) => (
-        <Typography
-          key={name}
-          as="li"
-          variant="small"
-          color="inherit"
-          className="capitalize"
-        >
-          {href ? (
-            <a
-              href={href}
-              target={target}
-              className="flex items-center gap-1 p-1 font-normal"
-            >
-              {icon &&
-                React.createElement(icon, {
-                  className: "w-[18px] h-[18px] opacity-75 mr-1",
-                })}
-              {name}
-            </a>
-          ) : (
-            <Link
-              to={path}
-              target={target}
-              className="flex items-center gap-1 p-1 font-normal"
-            >
-              {icon &&
-                React.createElement(icon, {
-                  className: "w-[18px] h-[18px] opacity-75 mr-1",
-                })}
-              {name}
-            </Link>
-          )}
-        </Typography>
-      ))}
-    </ul>
-  );
-
+export function Home() {
   return (
-    <MTNavbar color="transparent" className="p-3">
-      <div className="container mx-auto flex items-center justify-between text-white">
-        <Link to="/">
-          <Typography className="mr-4 ml-2 cursor-pointer py-1.5 font-bold">
-            {brandName}
-          </Typography>
-        </Link>
-        <div className="hidden lg:block">{navList}</div>
-        <div className="hidden gap-2 lg:flex">
-          <a
-            href="https://www.material-tailwind.com/blocks?ref=mtkr"
-            target="_blank"
-          >
-            <Button variant="text" size="sm" color="white" fullWidth>
-              pro version
-            </Button>
-          </a>
-          {React.cloneElement(action, {
-            className: "hidden lg:inline-block",
-          })}
+    <>
+      <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
+        <div className="absolute top-0 h-full w-full bg-[url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')] bg-cover bg-center" />
+        <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
+        <div className="max-w-8xl container relative mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
+              <Typography
+                variant="h1"
+                color="white"
+                className="mb-6 font-black"
+              >
+                Empowering the future of Education.
+              </Typography>
+              <Typography variant="lead" color="white" className="opacity-80">
+                "Stay ahead of the curve with AI-Driven Agile and Lean Education insights"
+              </Typography>
+            </div>
+          </div>
         </div>
-        <IconButton
-          variant="text"
-          size="sm"
-          color="white"
-          className="ml-auto text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <XMarkIcon strokeWidth={2} className="h-6 w-6" />
-          ) : (
-            <Bars3Icon strokeWidth={2} className="h-6 w-6" />
-          )}
-        </IconButton>
       </div>
-      <MobileNav
-        className="rounded-xl bg-white px-4 pt-2 pb-4 text-blue-gray-900"
-        open={openNav}
-      >
+      <section className="-mt-32 bg-gray-50 px-4 pb-20 pt-4">
         <div className="container mx-auto">
-          {navList}
-          <a
-            href="https://www.material-tailwind.com/blocks/react?ref=mtkr"
-            target="_blank"
-            className="mb-2 block"
-          >
-            <Button variant="text" size="sm" fullWidth>
-              pro version
-            </Button>
-          </a>
-          {React.cloneElement(action, {
-            className: "w-full block",
-          })}
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map(({ color, title, icon, description }) => (
+              <FeatureCard
+                key={title}
+                color={color}
+                title={title}
+                icon={React.createElement(icon, {
+                  className: "w-5 h-5 text-white",
+                })}
+                description={description}
+              />
+            ))}
+          </div>
+          <div className="mt-32 flex flex-wrap items-center">
+            <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+              <div className="mb-6 inline-flex h-16 w-16 items-center justify-center rounded-full bg-white p-3 text-center shadow-lg">
+                <UsersIcon className="h-6 w-6 text-blue-gray-900" />
+              </div>
+              <Typography
+                variant="h3"
+                className="mb-3 font-bold"
+                color="blue-gray"
+              >
+                Working with us is a pleasure
+              </Typography>
+              <Typography className="mb-8 font-normal text-blue-gray-500">
+                Don't let your uses guess by attaching tooltips and popoves to
+                any element. Just make sure you enable them first via
+                JavaScript.
+                <br />
+                <br />
+                The kit comes with three pre-built pages to help you get started
+                faster. You can change the text and images and you're good to
+                go. Just make sure you enable them first via JavaScript.
+              </Typography>
+              <Button variant="outlined">read more</Button>
+            </div>
+            <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+              <Card className="shadow-lg shadow-gray-500/10">
+                <CardHeader className="relative h-56">
+                  <img
+                    alt="Card Image"
+                    src="/img/teamwork.jpeg"
+                    className="h-full w-full"
+                  />
+                </CardHeader>
+                <CardBody>
+                  <Typography
+                    variant="h5"
+                    color="blue-gray"
+                    className="mb-3 font-bold"
+                  >
+                    Top Notch Services
+                  </Typography>
+                  <Typography className="font-normal text-blue-gray-500">
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens.
+                  </Typography>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
         </div>
-      </MobileNav>
-    </MTNavbar>
+      </section>
+      <section className="px-4 pt-20 pb-48">
+        <div className="container mx-auto">
+          <PageTitle heading="Here are our heroes">
+            According to the National Oceanic and Atmospheric Administration,
+            Ted, Scambos, NSIDClead scentist, puts the potentially record
+            maximum.
+          </PageTitle>
+          <div className="mt-24 grid grid-cols-1 gap-12 gap-x-24 md:grid-cols-2 xl:grid-cols-4">
+            {teamData.map(({ img, name, position, socials }) => (
+              <TeamCard
+                key={name}
+                img={img}
+                name={name}
+                position={position}
+                socials={
+                  <div className="flex items-center gap-2">
+                    {socials.map(({ color, name }) => (
+                      <IconButton key={name} color={color} variant="text">
+                        <i className={`fa-brands text-lg fa-${name}`} />
+                      </IconButton>
+                    ))}
+                  </div>
+                }
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="relative bg-blue-gray-50/50 py-24 px-4">
+        <div className="container mx-auto">
+          <PageTitle heading="Build something">
+            Put the potentially record low maximum sea ice extent tihs year down
+            to low ice. According to the National Oceanic and Atmospheric
+            Administration, Ted, Scambos.
+          </PageTitle>
+          <div className="mx-auto mt-20 mb-48 grid max-w-5xl grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
+            {contactData.map(({ title, icon, description }) => (
+              <Card
+                key={title}
+                color="transparent"
+                shadow={false}
+                className="text-center text-blue-gray-900"
+              >
+                <div className="mx-auto mb-6 grid h-14 w-14 place-items-center rounded-full bg-white shadow-lg shadow-gray-500/20">
+                  {React.createElement(icon, {
+                    className: "w-5 h-5",
+                  })}
+                </div>
+                <Typography variant="h5" color="blue-gray" className="mb-2">
+                  {title}
+                </Typography>
+                <Typography className="font-normal text-blue-gray-500">
+                  {description}
+                </Typography>
+              </Card>
+            ))}
+          </div>
+          <PageTitle heading="Want to work with us?">
+            Complete this form and we will get back to you in 24 hours.
+          </PageTitle>
+          <form className="mx-auto mt-12 max-w-3xl text-center">
+            <div className="mb-8 flex gap-8">
+              <Input variant="standard" size="lg" label="Full Name" />
+              <Input variant="standard" size="lg" label="Email Address" />
+            </div>
+            <Textarea variant="standard" size="lg" label="Message" rows={8} />
+            <Button variant="gradient" size="lg" className="mt-8">
+              Send Message
+            </Button>
+          </form>
+        </div>
+      </section>
+      <div className="bg-blue-gray-50/50">
+        <Footer />
+      </div>
+    </>
   );
 }
 
-Navbar.defaultProps = {
-  brandName: "Material Tailwind React",
-  action: (
-    <a
-      href="https://www.creative-tim.com/product/material-tailwind-kit-react"
-      target="_blank"
-    >
-      <Button variant="gradient" size="sm" fullWidth>
-        free download
-      </Button>
-    </a>
-  ),
-};
-
-Navbar.propTypes = {
-  brandName: PropTypes.string,
-  routes: PropTypes.arrayOf(PropTypes.object).isRequired,
-  action: PropTypes.node,
-};
-
-Navbar.displayName = "/src/widgets/layout/navbar.jsx";
-
-export default Navbar;
+export default Home;
